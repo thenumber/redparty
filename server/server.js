7c381bcc848c8640d215e5ba8b40bc2e117dae9f
Chris Nohr <chris@thenumber.ca>
2022-05-03 10:33:32 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const ioUtils = require('./utils/io');
+const server = require("http").createServer(app);
+const ioUtils = require("./utils/io");
 
-const io = require('socket.io')(server, {
-	path: '/socket',
-	origins: ['https://redparty.netlify.app'],
-	serveClient: false,
+const io = require("socket.io")(server, {
+  path: "/socket",
+  origins: ["https://interconnector-youtube.netlify.app"],
+  serveClient: false,
 });
 
 const PORT = process.env.PORT || 3005;
 
-app.get('/test', (req, res, next) => {
-	res.send({ message: 'Hello World' });
+app.get("/test", (req, res, next) => {
+  res.send({ message: "Hello World" });
 });
 
 ioUtils.setupIO(io);
